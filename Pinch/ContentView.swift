71b6128b1ec4e1552fc451204266bac8891b761c
Danqing Liu <git@danqing.io>
2023-01-10 14:40:46 -0500

    Initial Commit

---
+++ Pinch/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Danqing Liu on 1/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Danqing Liu on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
